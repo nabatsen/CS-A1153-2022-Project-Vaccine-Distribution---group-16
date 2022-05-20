f16592d49119d68aefa893155219ca5742348ba7
Shaban Sahar <sahar.shaban@aalto.fi>
2022-05-20 17:25:24 +0300

    Fixed tables

--- code/tables.sql
+++ code/tables.sql
@@ -13,6 +13,7 @@ CREATE TABLE ProductionFacility(
 
 CREATE TABLE Vaccine(
     id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
     requiredDoses INTEGER NOT NULL,
     tempMin INTEGER NOT NULL,
     tempMax INTEGER NOT NULL
@@ -28,26 +29,27 @@ CREATE TABLE License(
     PRIMARY KEY(manufacturer, vaccine)
 );
 
-CREATE TABLE Batch(
-    id VARCHAR PRIMARY KEY,
-    vaccine VARCHAR NOT NULL REFERENCES Vaccine(id) ON UPDATE CASCADE,
-    amount INTEGER NOT NULL,
-    manufacturer VARCHAR NOT NULL REFERENCES ProductionFacility(manufacturer) ON UPDATE CASCADE,
-    country VARCHAR NOT NULL REFERENCES ProductionFacility(country),
-    productionDate VARCHAR NOT NULL,
-    expiryDate VARCHAR NOT NULL
-);
-
 CREATE TABLE VaccinationPoint(
     name VARCHAR PRIMARY KEY,
     address VARCHAR NOT NULL,
     phone VARCHAR NOT NULL
 );
 
+
+CREATE TABLE Batch(
+    id VARCHAR PRIMARY KEY,
+    vaccine VARCHAR NOT NULL REFERENCES Vaccine(id) ON UPDATE CASCADE,
+    amount INTEGER NOT NULL,
+    manufacturer VARCHAR NOT NULL REFERENCES Manufacturer(id) ON UPDATE CASCADE,
+    productionDate VARCHAR NOT NULL,
+    location VARCHAR REFERENCES VaccinationPoint(name),
+    expiryDate VARCHAR NOT NULL
+);
+
 CREATE TABLE TransportationLog(
     batch VARCHAR REFERENCES Batch(id),
-    departureDate VARCHAR,
-    departurePoint VARCHAR REFERENCES VaccinationPoint(name) ON UPDATE CASCADE,
+    departureDate VARCHAR NOT NULL,
+    departurePoint VARCHAR NOT NULL REFERENCES VaccinationPoint(name) ON UPDATE CASCADE,
     arrivalDate VARCHAR,
     arrivalPoint VARCHAR REFERENCES VaccinationPoint(name) ON UPDATE CASCADE,
     PRIMARY KEY(batch, arrivalDate, arrivalPoint)
@@ -59,52 +61,53 @@ CREATE TABLE Employee(
     birthday VARCHAR NOT NULL,
     phone VARCHAR NOT NULL,
     vaccinationStatus INTEGER NOT NULL,
-    role VARCHAR NOT NULL CHECK(role IN ("doctor", "nurse"))
+    role VARCHAR NOT NULL CHECK(role IN ('doctor', 'nurse')),
+    vaccinationPoint VARCHAR NOT NULL REFERENCES VaccinationPoint(name)
 );
 
-CREATE DOMAIN WeekdayDomain VARCHAR CHECK(VALUE IN ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"));
+CREATE DOMAIN WeekdayDomain VARCHAR CHECK(VALUE IN ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'));
 
 CREATE TABLE Shift(
-    vaccinationPoint REFERENCES VaccinationPoint(name)
+    vaccinationPoint VARCHAR REFERENCES VaccinationPoint(name)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     weekday WeekdayDomain,
-    employee REFERENCES Employee(ssNo) ON DELETE CASCADE,
+    employee VARCHAR REFERENCES Employee(ssNo) ON DELETE CASCADE,
     PRIMARY KEY(vaccinationPoint,weekday,employee)
 );
 
 CREATE TABLE VaccinationEvent(
     date VARCHAR,
-    point VARCHAR REFERENCES VaccinationPoint(name)
+    vaccinationPoint VARCHAR REFERENCES VaccinationPoint(name)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     batch VARCHAR NOT NULL REFERENCES Batch(id),
-    PRIMARY KEY(date, point)
+    PRIMARY KEY(date, vaccinationPoint)
 );
 
 CREATE TABLE Patient(
     ssNo VARCHAR PRIMARY KEY,
     name VARCHAR NOT NULL,
     birthday VARCHAR NOT NULL,
-    gender CHAR(1) NOT NULL CHECK(gender IN ('F', 'M', 'O')),
-    vaccinationStatus INTEGER NOT NULL,
+    gender CHAR(1) NOT NULL CHECK(gender IN ('F', 'M', 'O'))
 );
 
 CREATE TABLE VaccinationAppointment(
     patient VARCHAR REFERENCES Patient(ssNo) ON DELETE CASCADE,
-    date VARCHAR REFERENCES VaccinationEvent(date)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    point VARCHAR REFERENCES VaccinationEvent(point)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    PRIMARY KEY (patient, date, point)
+    date VARCHAR NOT NULL,
+    point VARCHAR NOT NULL,
+    PRIMARY KEY(patient, date, point),
+    FOREIGN KEY(date, point) REFERENCES VaccinationEvent(date, vaccinationPoint)
 );
 
 CREATE TABLE Symptom(
+    name VARCHAR PRIMARY KEY,
+    critical BOOLEAN NOT NULL
+);
+
+CREATE TABLE Diagnosis(
     patient VARCHAR REFERENCES Patient(ssNo) ON DELETE CASCADE,
-    symptom VARCHAR,
+    symptom VARCHAR REFERENCES Symptom(name),
     date VARCHAR,
-    critical BOOLEAN NOT NULL,
     PRIMARY KEY(patient, symptom, date)
 );
