09cd3f18445fafe9efee81176df50ccf4d47be8f
Nhut (Nana) Cao <nhutmcao@gmail.com>
2022-05-22 00:57:36 +0300

    Update query 6&7

--- code/queries.sql
+++ code/queries.sql
@@ -25,3 +25,23 @@ WHERE id = batch AND name = arrivalPoint AND NOT EXISTS(
     WHERE TransportationLog.batch = T.batch AND TransportationLog.arrivalDate > T.arrivalDate
 ) AND location != arrivalPoint
 ORDER BY id;
+
+-- Query 6
+SELECT location AS "Hospital/Clinic", name AS vaccine, total AS "No. of vaccines of different types", SUM(total) OVER (PARTITION BY location) AS "No. of Vaccine" 
+FROM(SELECT location, Vaccine.name, SUM(amount) AS total 
+        FROM Batch JOIN Vaccine ON Vaccine.id = Batch.vaccine 
+        GROUP BY location, Vaccine.name) AS tempTable;
+
+--Query 7
+WITH Tables AS(SELECT VA.patient as patientid, Vaccine.name, symptom FROM VaccinationAppointment VA 
+    JOIN VaccinationEvent VE ON VE.date = VA.date AND VE.vaccinationPoint = VA.vaccinationPoint
+    JOIN Batch ON Batch.id = VE.batch
+    JOIN Vaccine ON Vaccine.id = Batch.vaccine
+    JOIN Diagnosis D ON D.patient = VA.patient AND D.date > VA.date), 
+    Calculation AS(SELECT name, COUNT(DISTINCT(tables.patientid)) AS total 
+        FROM Tables GROUP BY name),
+    Types AS(SELECT name, symptom, COUNT(DISTINCT(tables.patientid)) AS total 
+        FROM Tables GROUP BY name, symptom) 
+SELECT Types.name AS "Vaccine", Types.symptom, 
+ROUND(Types.total*1.0/Calculation.total, 6) AS "Frequency"
+FROM Types JOIN Calculation ON Types.name = Calculation.name;
