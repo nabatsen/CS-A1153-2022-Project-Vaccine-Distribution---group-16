034823bc389076d621b83e43d7964b9ab81e730f
Nhut (Nana) Cao <nhutmcao@gmail.com>
2022-05-31 14:24:39 +0300

    Task 7 done

--- code/part3.py
+++ code/part3.py
@@ -110,7 +110,6 @@ def main():
         print("------------------------------------------------------------------------------------------------------")
 
         # Task 7
-        # Have not changed to label
         Frequency = pd.read_sql_query("""WITH Tables AS(SELECT VA.patient as patientid, Vaccine.name, VA.date
                                         FROM VaccinationAppointment VA
                                         JOIN VaccinationEvent VE ON VE.date = VA.date AND VE.vaccinationPoint = VA.vaccinationPoint
@@ -134,16 +133,21 @@ def main():
         FreqAstra = Frequency[:13].reset_index().drop(columns=['index', 'Vaccine']).sort_values(by=['symptom'])
         FreqComirnaty = Frequency[14:23].reset_index().drop(columns=['index', 'Vaccine']).sort_values(by=['symptom'])
         FreqModerna = Frequency[24:].reset_index().drop(columns=['index', 'Vaccine']).sort_values(by=['symptom'])
-        print('-'*20)
-        print(Frequency)
         merge_df = pd.merge(Symptoms, FreqAstra, how='left', on='symptom')
         merge_df = pd.merge(merge_df, FreqModerna, how='left', on='symptom')
         merge_df = pd.merge(merge_df, FreqComirnaty, how='left', on='symptom')
         merge_df.rename(columns={"Frequency_x": "V01", "Frequency_y": "V02", "Frequency": "V03"}, inplace=True)
         cols = ['V01', 'V02', 'V03']
         merge_df[cols] = merge_df[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+        merge_df['V01'] = pd.cut(merge_df['V01'], [0.0, 0.05, 0.1, 1], labels=["rare", "common", "very common"],
+                                 right=False).values.add_categories('-')
+        merge_df['V02'] = pd.cut(merge_df['V02'], [0.0001, 0.05, 0.1, 1], labels=["rare", "common", "very common"],
+                                 right=False).values.add_categories('-')
+        merge_df['V03'] = pd.cut(merge_df['V03'], [0.0001, 0.05, 0.1, 1], labels=["rare", "common", "very common"],
+                                 right=False).values.add_categories('-')
 
         print(merge_df.fillna('-'))
+        print("------------------------------------------------------------------------------------------------------")
     except Exception as e:
         print(e)
     finally:
